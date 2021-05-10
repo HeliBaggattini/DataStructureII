2d2adbbedcefc9a3eb38665132b93989721fece9
Heli <Heli_B@users.noreply.github.com>
2021-05-10 19:07:37 -0300

    Update on binary search tree

--- DataStructureII/src/BinarySearchTree/BinarySearchTree.java
+++ DataStructureII/src/BinarySearchTree/BinarySearchTree.java
@@ -7,6 +7,7 @@ package BinarySearchTree;
 
 import BinaryTree.BinaryTree;
 import BinaryTree.Node;
+import java.util.ArrayList;
 
 /**
  *
@@ -57,35 +58,92 @@ public class BinarySearchTree extends BinaryTree {
     }
 //6. Escreva um método que permita inserir elementos de forma ordenada em uma árvore binária de
 //busca.
+
     public void addNode(String element) {
         boolean ok = false;
         Node n = this.getRoot();
 
         while (!ok) {
             if (n.element.compareTo(element) > 1) { //if smaller
-                if(n.left == null){
+                if (n.left == null) {
                     n.left = new Node(element);
                     ok = true;
-                }else{
+                } else {
                     n = n.left;
                 }
             } else if (n.element.compareTo(element) == 0) {//if equals
-                if(n.left == null){
+                if (n.left == null) {
                     n.left = new Node(element);
                     ok = true;
-                }else{
+                } else {
                     n = n.left;
                 }
             } else if (n.element.compareTo(element) < 1) {//if bigger
-                if(n.right == null){
+                if (n.right == null) {
                     n.right = new Node(element);
                     ok = true;
-                }else{
+                } else {
                     n = n.right;
                 }
             }
         }
 
+    }
+
+    //7. Escreva um método que retorna verdadeiro se uma árvore é binária de busca e falso caso
+    //contrário.
+    public boolean isSearchTree(Node n) {
+        boolean ok = true;
+
+        if (n == null) {
+            return false;
+        }
+
+        if (n.left != null) {
+            if (n.element.compareTo(n.left.element) >= 0) {
+                ok = isSearchTree(n.left);
+            } else {
+                return false;
+            }
+        }
+
+        if (n.right != null) {
+            if (n.element.compareTo(n.right.element) < 1) {
+                ok = isSearchTree(n.right);
+            } else {
+                return false;
+            }
+        }
+
+        return ok;
+    }
+
+    //8. Escreva um método que permita remover elementos de uma árvore binária de busca.
+    public void removeNode(String element) {
+        //unfinished
+    }
+
+    //9. Escreva um programa que converte uma árvore binária em uma árvore binária de busca.
+    public BinarySearchTree toBinarySearchTree(BinaryTree binaryTree) {
+        BinarySearchTree bst = null;
+        ArrayList<Node> l = binaryTree.inOrderArray(binaryTree.getRoot());
+        
+        //for(Node n: binaryTree.inOrderArray(binaryTree.getRoot())){
+        for(int i = 0; i <= (l.size() - 1); i++){
+            if(bst == null){
+                bst = new BinarySearchTree(l.get(i).element);
+            }else{
+                bst.addNode(l.get(i).element);
+            }       
+        }
+        
+        return bst;
+    }
+
+    //10. Escreva um algoritmo que cria uma imagem espelho de uma árvore binária, isto é, todos os
+    //filhos à esquerda tornam-se filhos à direita, e vice-versa
+    public BinarySearchTree mirror(BinarySearchTree tree) {
+        return null;//unfinished
     }
 
 }
--- DataStructureII/src/BinarySearchTree/test/BSTtest.java
+++ DataStructureII/src/BinarySearchTree/test/BSTtest.java
@@ -6,24 +6,62 @@
 package BinarySearchTree.test;
 
 import BinarySearchTree.BinarySearchTree;
+import BinaryTree.BinaryTree;
+import BinaryTree.Node;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author 1511 FOX
  */
 public class BSTtest {
+
     public static void main(String[] args) {
-         BinarySearchTree bst = new BinarySearchTree("h");
-         bst.addNode("a");
-         bst.addNode("b");
-         bst.addNode("i");
-         bst.addNode("j");
-         bst.addNode("c");
-         bst.addNode("h");
-         
-         System.out.println(bst.preOrder(bst.getRoot()));
-         System.out.println(bst.nonRecursiveSmallNum(bst.getRoot()));
-         System.out.println(bst.search(bst.getRoot(), "c"));
-         System.out.println(bst.search(bst.getRoot(), "x"));
+        BinarySearchTree bst = new BinarySearchTree("h");
+
+        System.out.println("isSearch  = " + bst.isSearchTree(bst.getRoot()));
+
+        //forcing the tree to not be search tree
+//        try {
+//            bst.addChildLeft(bst.getRoot(), "x");
+//        } catch (Exception ex) {
+//            Logger.getLogger(BSTtest.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        bst.addNode("a");
+        bst.addNode("b");
+        bst.addNode("i");
+        bst.addNode("j");
+        bst.addNode("c");
+        bst.addNode("h");
+
+        System.out.println("tree: " + bst.preOrder(bst.getRoot()));
+        System.out.println("Smallest number (non recursive): " + bst.nonRecursiveSmallNum(bst.getRoot()));
+        System.out.println("Search for 'c': " + bst.search(bst.getRoot(), "c"));
+        System.out.println("Search for 'x': " + bst.search(bst.getRoot(), "x"));
+        System.out.println("isSearch  = " + bst.isSearchTree(bst.getRoot()));
+
+        //creating a binary tree
+        BinaryTree bt = new BinaryTree("root");
+        //adding nodes to the tree
+        try {
+            Node a = bt.addChildLeft(bt.getRoot(), "a");
+            Node b = bt.addChildRight(bt.getRoot(), "k");
+            Node c = bt.addChildLeft(a, "c");
+            Node d = bt.addChildRight(a, "d");
+            Node e = bt.addChildLeft(b, "e");
+            Node f = bt.addChildRight(b, "i");
+            Node g = bt.addChildLeft(c, "g");
+            Node h = bt.addChildRight(c, "h");
+            Node i = bt.addChildRight(d, "i");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        //converting binary tree to search binary tree
+        BinarySearchTree bst2 = new BinarySearchTree("a");
+        bst2 = bst2.toBinarySearchTree(bt);
+        System.out.println(bst2.preOrder(bst2.getRoot()));
     }
+
 }
